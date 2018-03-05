2dbf54b347bdddf52154afb244cf3056e7398604
Nathan Chancellor <natechancellor@gmail.com>
2018-03-05 16:18:43 -0700

    Pixel 2: anykernel.sh: Bump supported configuration to March security update
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -54,7 +54,7 @@ android_version="$(system_getprop "ro.build.version.release")";
 security_patch="$(system_getprop "ro.build.version.security_patch")";
 version_info="$android_version:$security_patch";
 case "$version_info" in
-    "8.1.0:2018-02-05") support_status="a supported";;
+    "8.1.0:2018-03-05") support_status="a supported";;
     *) support_status="an unsupported";;
 esac;
 ui_print " "; ui_print "You are on $android_version with the $security_patch security patch level! This is $support_status configuration..."
