e173f0ac2823f61a6e2de3c7e84758a18f79237a
Nathan Chancellor <natechancellor@gmail.com>
2018-05-07 16:50:50 -0700

    Pixel 2: Update supported configuration to May security patch level
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -50,7 +50,7 @@ android_version="$(file_getprop /system/build.prop "ro.build.version.release")";
 security_patch="$(file_getprop /system/build.prop "ro.build.version.security_patch")";
 version_info="$android_version:$security_patch";
 case "$version_info" in
-    "8.1.0:2018-04-01"|"8.1.0:2018-04-05"|"P:2018-03-05") support_status="a supported";;
+    "8.1.0:2018-05-01"|"8.1.0:2018-05-05"|"P:2018-03-05") support_status="a supported";;
     *) support_status="an unsupported";;
 esac;
 ui_print " "; ui_print "You are on $android_version with the $security_patch security patch level! This is $support_status configuration..."
