02baf2165d7c2c9ff581336f4d8bfe897226a986
Nathan Chancellor <natechancellor@gmail.com>
2018-07-03 15:11:44 -0700

    op6: anykernel.sh: Drastically slim down
    * /system_root and library path handling has been moved into the
      backend.
    
    * /sbin -> /sbin_tmp is not necessary for magiskboot, meaning we no
      longer need to check if we are in recovery or not..
    
    * Just check for existence of a concatenated image directly.
    
    * die isn't needed, it can be readded later if so.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -19,34 +19,6 @@ is_slot_device=1;
 ramdisk_compression=auto;
 
 
-# Detect whether we're in recovery or booted up
-ps | grep zygote | grep -v grep >/dev/null && in_recovery=false || in_recovery=true;
-! $in_recovery || ps -A 2>/dev/null | grep zygote | grep -v grep >/dev/null && in_recovery=false;
-! $in_recovery || id | grep -q 'uid=0' || in_recovery=false;
-
-
-# Unmount system and restore /sbin and library paths
-restore_recovery() {
-  if $in_recovery; then
-    mv /sbin_tmp /sbin 2>/dev/null;
-    [ -z $OLD_LD_LIB ] || export LD_LIBRARY_PATH=$OLD_LD_LIB;
-    [ -z $OLD_LD_PRE ] || export LD_PRELOAD=$OLD_LD_PRE;
-    umount /system;
-    umount /system_root;
-    rmdir /system_root;
-    mount -o ro -t auto /system;
-  fi;
-}
-
-
-# Do recovery restore, print message, and exit
-die() {
-  restore_recovery;
-  ui_print " "; ui_print "$*";
-  exit 1;
-}
-
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
@@ -56,35 +28,10 @@ die() {
 dump_boot;
 
 
-# Find image setup
+# If the kernel image and dtbs are separated in the zip
 decompressed_image=/tmp/anykernel/kernel/Image
 compressed_image=$decompressed_image.gz
 if [ -f $compressed_image ]; then
-  concatenated_image=false;
-else
-  concatenated_image=true;
-fi;
-
-
-if $in_recovery; then
-  # magiskboot is linked against /system/bin/linker
-  umount /system;
-  umount /system 2>/dev/null;
-  mkdir /system_root 2>/dev/null;
-  mount -o ro -t auto /dev/block/bootdevice/by-name/system$slot /system_root;
-  mount -o bind /system_root/system /system;
-  # Temporarily block out all custom recovery binaries/libs
-  mv /sbin /sbin_tmp;
-  # Unset library paths
-  OLD_LD_LIB=$LD_LIBRARY_PATH;
-  OLD_LD_PRE=$LD_PRELOAD;
-  unset LD_LIBRARY_PATH;
-  unset LD_PRELOAD;
-fi;
-
-
-# If the user does not supply a concatenated image
-if ! $concatenated_image; then
   # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
   if [ -d $ramdisk/.backup ]; then
     ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
@@ -96,10 +43,6 @@ if ! $concatenated_image; then
   # Concatenate all of the dtbs to the kernel
   cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
 fi;
-
-
-# Restore recovery if applicable
-restore_recovery;
 
 
 # Install the boot image
