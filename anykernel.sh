bdac8da6294fa1e1850fc383e49335a2da681038
Nathan Chancellor <natechancellor@gmail.com>
2018-09-05 23:43:13 -0700

    op6: anykernel.sh: Prevent users from flashing on Pie
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -23,6 +23,15 @@ ramdisk_compression=auto;
 . /tmp/anykernel/tools/ak2-core.sh;
 
 
+# Save the users from themselves
+android_version="$(file_getprop /system/build.prop "ro.build.version.release")";
+supported_version=8.1.0;
+if [ "$android_version" != "$supported_version" ]; then
+  ui_print " "; ui_print "You are on $android_version but this kernel is only for $supported_version!";
+  exit 1;
+fi;
+
+
 ## AnyKernel install
 dump_boot;
 
