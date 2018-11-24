c29edbdb7bcb974914f2c0b5a979ccf7e4297af5
wrongway213 <tropeazy@gmail.com>
2018-11-24 07:01:21 +0530

    AnyKernel2: This is Krieg Kernel, now!

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Flash Kernel for the Pixel 2 (XL) by @nathanchance
+kernel.string=Krieg Kernel for the Pixel 2 (XL) by wrongway213, APOPHIS9283, yoinx
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
